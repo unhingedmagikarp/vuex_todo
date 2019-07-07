36eda329bce62c16cc45cff31d42e8130a05c1c7
peter-keller <keller.f.peter@gmail.com>
2019-07-07 23:40:58 +0100

    init app, get and post todos

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <AddTodo />
+      <Todos />
+    </div>
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+
+export default {
+  name: "app",
+  components: {
+    Todos,
+    AddTodo
+  }
+};
+</script>
+
+<style>
+body {
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
