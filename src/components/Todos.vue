d3b6e2529df16fe628d6aa4163aa68f538c8a9b3
peter-keller <keller.f.peter@gmail.com>
2019-07-08 16:17:06 +0100

    completing todos

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
     <h3>Todos</h3>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span>
+        <span class="incomplete-box"></span> = Incomplete
+      </span>
+      <span>
+        <span class="complete-box"></span> = Complete
+      </span>
+    </div>
     <div class="todos">
-      <div v-for="todo in allTodos" :key="todo.id" class="todo">
+      <div @dblclick="onDoubleClick(todo)" v-for="todo in allTodos" :key="todo.id" class="todo" v-bind:class="{'is-complete': todo.completed}">
         {{ todo.title }}
         <i @click="deleteTodo(todo.id)" class="fas fa-trash"></i>
       </div>
@@ -15,11 +24,20 @@ import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "Todos",
-  methods: { ...mapActions(["fetchTodos", "deleteTodo"]) },
+  methods: { ...mapActions(["fetchTodos", "deleteTodo", 'updateTodo']),
+    onDoubleClick (todo) {
+      const selectedTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed
+      }
+      this.updateTodo(selectedTodo)
+    }
+  },
   computed: mapGetters(["allTodos"]),
   created() {
     this.fetchTodos();
-  }
+  },
 };
 </script>
 
@@ -47,4 +65,36 @@ i {
   color: white;
   cursor: pointer;
 }
+
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+
+.is-complete {
+  background: #35495e;
+  color: white;
+}
+
+@media(max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
+}
 </style>
+
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -34,6 +34,10 @@ const actions = {
       `https://jsonplaceholder.typicode.com/todos?_limit=${limit}`
     );
     commit("setTodos", response.data);
+  },
+  async updateTodo({commit}, todo) {
+    await axios.patch(`https://jsonplaceholder.typicode.com/todos/${todo.id}`)
+    commit('upTodo', todo)
   }
 };
 
@@ -41,7 +45,13 @@ const mutations = {
   setTodos: (state, todos) => (state.todos = todos),
   newTodo: (state, todo) => state.todos.unshift(todo),
   removeTodo: (state, id) =>
-    (state.todos = state.todos.filter(todo => todo.id !== id))
+    (state.todos = state.todos.filter(todo => todo.id !== id)),
+  upTodo: (state, selectedTodo) => {
+    const index = state.todos.findIndex(todo => todo.id === selectedTodo.id)
+    if (index !== -1) {
+      state.todos.splice(index, 1, selectedTodo)
+    }
+  }
 };
 
 export default {
